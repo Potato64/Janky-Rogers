3a128e0686808d324723232985706ddd74b8c667
4089Software <stealthrobotics@gmail.com>
2019-09-30 15:31:44 -0700

    Update DriveBase.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBase.java
@@ -3,12 +3,19 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import static java.lang.Math.PI;
+import static java.lang.Math.abs;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
 public class DriveBase
 {
     DcMotor frMotor;
     DcMotor flMotor;
     DcMotor brMotor;
     DcMotor blMotor;
+
+    private static final double wCoef = 1/(sin(23 * PI/36));
 
     public DriveBase(HardwareMap hardwareMap)
     {
@@ -20,6 +27,24 @@ public class DriveBase
 
     public void drive(double speed, double angle, double rot)
     {
+        double flbr = flbrPower(angle);
+        double frbl = frblPower(angle);
 
+        double maxPower = speed * (abs(flbr) > abs(frbl) ? abs(flbr) : abs(frbl)) + abs(rot);
+
+        frMotor.setPower((frbl * speed - rot) / maxPower);
+        flMotor.setPower((flbr * speed + rot) / maxPower);
+        blMotor.setPower((frbl * speed + rot) / maxPower);
+        brMotor.setPower((flbr * speed - rot) / maxPower);
+    }
+
+    private double flbrPower(double angle)
+    {
+        return wCoef * sin(angle - 23 * PI / 36);
+    }
+
+    private double frblPower(double angle)
+    {
+        return wCoef * cos(angle - 5 * PI / 36);
     }
 }
