9588c3a043f00b5b9c87ccb60d2cead024e702e7
4089Software <stealthrobotics@gmail.com>
2019-10-02 17:37:23 -0700

    worked on sensors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorREVColorDistance;
+
+public class Sensors
+{
+    SensorREVColorDistance bottomColor1;
+    SensorREVColorDistance bottomColor2;
+
+    SensorREVColorDistance sideL;
+    SensorREVColorDistance sideR;
+
+    SensorREVColorDistance frontDist1;
+    SensorREVColorDistance frontDist2;
+    SensorREVColorDistance frontDist3;
+
+    public Sensors()
+    {
+
+    }
+}
