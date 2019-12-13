f0f054492f33b15252560756d6d6fea165c1757e
4089Software <stealthrobotics@gmail.com>
2019-12-13 14:03:16 -0800

    worked on bb auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpBB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpBB.java
@@ -2,19 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.Math.PI;
 
 @Autonomous
-@Disabled
 public class AutoOpBB extends LinearOpMode
 {
     // Declare OpMode members.
@@ -29,29 +25,6 @@ public class AutoOpBB extends LinearOpMode
     private BNO055IMU imu;
     private Sensors sensors;
 
-    private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
-    private static final String LABEL_FIRST_ELEMENT = "Stone";
-    private static final String LABEL_SECOND_ELEMENT = "Skystone";
-
-    private static final String VUFORIA_KEY =  "AdcmR0H/////AAABmX5FCuasR0WJva" +
-            "W+WfKBZZY4Mkmr39mE5zeNXTSqAfbDNYf8W53AS1tK2Fgrwh1kN/CVmyZnbLlKxrn" +
-            "I9kDObs2/9mddaFBkwb7TCKrCo4Cy/kqF85YWtWYekU3EqHzuMNvw1OonPzQJ7kjgB" +
-            "hszai3nQKg5bN2dd8NBPFpScozkSlvIUrWFvbj20K1K7kQ3JjgMreeSNldB12C+Zeee" +
-            "Ui9IVDBqfkcBOszPh0HvSMBGX3IkIkac56/UcTFbaa/GNWTwZtrVshjnypXm15Y1d62E" +
-            "hg9G8wNHFMwhvzHqfGvLA++K7x/dCtB+iaPE7WNxom4RaE+UYJ8kDjbpjrbtiXPuqUN9+SbCwQK6IN86vAer";
-
-    /**
-     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
-     * localization engine.
-     */
-    private VuforiaLocalizer vuforia;
-
-    /**
-     * {@link #tfod} is the variable we will use to store our instance of the TensorFlow Object
-     * Detection engine.
-     */
-    private TFObjectDetector tfod;
-
     /*
      * Code to run ONCE when the driver hits INIT
      */
@@ -60,34 +33,15 @@ public class AutoOpBB extends LinearOpMode
     {
         driveBase = new DriveBase(hardwareMap);
 
+        driveBase.setImuStabililzed(true);
+        driveBase.setHeadless(true);
+
         lift = new Lift(hardwareMap);
         claw = hardwareMap.get(DcMotor.class, "claw");
 
         imu = hardwareMap.get(BNO055IMU.class, "imu");
 
         sensors = new Sensors(hardwareMap);
-
-        // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
-        // first.
-        initVuforia();
-
-        if (ClassFactory.getInstance().canCreateTFObjectDetector())
-        {
-            initTfod();
-        }
-        else
-        {
-            telemetry.addData("Sorry!", "This device is not compatible with TFOD");
-        }
-
-        /**
-         * Activate TensorFlow Object Detection before we wait for the start command.
-         * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
-         **/
-        if (tfod != null)
-        {
-            tfod.activate();
-        }
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
@@ -96,50 +50,36 @@ public class AutoOpBB extends LinearOpMode
         waitForStart();
         runtime.reset();
 
-        driveBase.drive(1, PI/4, 0);
+        double now = runtime.milliseconds();
+
+        while (opModeIsActive() && runtime.milliseconds() - now < 2000)
+        {
+            driveBase.drive(1, 0, 0);
+        }
+
+        driveBase.drive(0, 0, 0.5);
+
+        while(opModeIsActive() && driveBase.getAngle() < PI/2 - 0.1);
+
+        driveBase.setStablilizedHeading(PI/2);
+
+        while(opModeIsActive() && sensors.getFrontDistance() > 4)
+        {
+            driveBase.drive(0.07 + (sensors.getFrontDistance() - 3) * 0.1, 0, 0);
+        }
+
+        lift.setPower(0.3);
+        sleep(1500);
+        lift.setPower(0);
+
+        now = runtime.milliseconds();
+        while (opModeIsActive() && runtime.milliseconds() - now < 5000)
+        {
+            driveBase.drive(1, PI, 0);
+        }
+
+        driveBase.drive(-1, -PI/2, 0);
 
         sleep(500);
-
-        driveBase.drive(1, 0, 0);
-
-        while(opModeIsActive() && sensors.getFrontDistance() > 1);
-
-        driveBase.drive(-1, 0, 0);
-
-        sleep(5000);
-
-        driveBase.drive(1, -PI/2, 0);
-
-        while (opModeIsActive() && sensors.getBottomC() < 100);
-    }
-
-    /**
-     * Initialize the Vuforia localization engine.
-     */
-    private void initVuforia() {
-        /*
-         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
-         */
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-
-        //  Instantiate the Vuforia engine
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-
-        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
-    }
-
-    /**
-     * Initialize the TensorFlow Object Detection engine.
-     */
-    private void initTfod() {
-        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minimumConfidence = 0.8;
-        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
     }
 }
